2cec1f2b2fd51dd554f18ebacf97074c260148b7
Madalin Novac <67506810+MadalinNov@users.noreply.github.com>
2020-08-12 19:57:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 var burgerMenu = document.querySelector('.burger');
 var sideNav = document.querySelector('.nav-links');
 var backTop = document.querySelector('.backTop');
+var cart = document.querySelector('.cart');
+var addToCart=document.querySelector('.cta-buy');
+var productAddToCart = document.querySelectorAll('.addtoCart');
+var btnsArr = Array.prototype.slice.call(productAddToCart);
 // EVENT LISTENERS
 var setEventListeners = (function() {
   burgerMenu.addEventListener('click', function() {
@@ -9,7 +13,19 @@ var setEventListeners = (function() {
     burgerMenu.classList.toggle('burger-active');
     sideNav.classList.toggle('nav-links-active');
   });
+  //Cart Notiffication
+
+  addToCart.addEventListener('click', function(){
+    cart.classList.add('addToCartActive');
+  });
+
+  for(var i=0;i<btnsArr.length;i++){
+    btnsArr[i].addEventListener('click', function(){ //JSHint ignoreline
+      cart.classList.add('addToCartActive');
+    });
+  }
   //SMOOTH SCROLL
+
   $(document).ready(function() {
     $("a").on('click', function(event) {
       if (this.hash !== "") {
@@ -23,6 +39,7 @@ var setEventListeners = (function() {
       }
     });
   });
+
   //Section check
   $(window).scroll(function(){
     var scrollY=$(window).scrollTop();
